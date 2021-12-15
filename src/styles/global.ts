6cbafd2695235d54dcd12c640482d749feeba702
danyelvarejao <danyelvarejao@gmail.com>
2021-12-15 19:03:20 -0300

    Added styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    background-color: #181818;
+    color: #fff;
+  }
+`
+
+export default GlobalStyles
