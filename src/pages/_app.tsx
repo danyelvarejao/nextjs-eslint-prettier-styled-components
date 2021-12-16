14b787dc8a0bdb1dcaa56d7a2834661c22b4b982
danyelvarejao <danyelvarejao@gmail.com>
2021-12-16 16:43:29 -0300

    Added path @ in tsconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from 'styles/global'
-import dark from 'styles/themes/dark'
+
+import GlobalStyles from '@/styles/global'
+import dark from '@/styles/themes/dark'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
